4ec4c54707d6837a03fc672b60d11a298204ecee
Rahul Goel <rahul.usit12@gmail.com>
2018-06-02 00:57:32 +0530

    setting up of log4j2

--- src/main/java/payment_gateway/PaymentGateway.java
+++ src/main/java/payment_gateway/PaymentGateway.java
@@ -1,9 +1,22 @@
 package payment_gateway;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Rahul Goel created on 2/6/18
  */
 
-
+@SpringBootApplication
 public class PaymentGateway {
+
+    private static Logger logger = LogManager.getLogger(PaymentGateway.class);
+
+    public static void main(String[] args) {
+        logger.info("Starting payment-gateway application");
+        SpringApplication.run(PaymentGateway.class, args);
+        logger.info("Payment-gateway application started");
+    }
 }
